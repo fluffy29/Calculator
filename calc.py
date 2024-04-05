9f40f2ad42ea19bb22bc83bb48131b53a44afce2
fluffy29 <hmajzoub12@gmail.com>
2024-04-05 18:27:46 +0200

    Rewrote it

--- calc.py
+++ calc.py
@@ -1,24 +1,24 @@
-def add(a, b):
-  return a + b
+def add (a, b):
+  add = a + b
+  return add
+print(add(3, 4))
 
-def subtract(a, b):
-  return a - b
+def subtract (a, b):
+  subtract = a - b
+  return subtract
+print(subtract(3, 4))
 
-def multiply(a, b):
-  return a * b
+def multiply (a, b):
+  multiply = a * b
+  return multiply
+print(multiply(3, 4))
 
-def divide(a, b):
-  return a / b
+def divide (a, b):
+  divide = a / b
+  return divide
+print(divide(3, 4))
 
-def exp(a, b):
-  return a ** b
-
-print(add(3, 5))
-
-print(subtract(7, 2))
-
-print(multiply(4, 8))
-
-print(divide(9, 3))
-
-print(exp(2, 3))
+def exp (a, b):
+  exp = a ** b
+  return exp
+print(exp(3, 4))
